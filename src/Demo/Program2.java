81a090a4468f384ac5b28cf7baa3742a22c6617b
Nishi Bharti <iamnishibharti@gmail.com>
2021-09-01 22:01:07 +0530

    Nishi_Branch_A

---
+++ src/Demo/Program2.java
@@ -0,0 +1,26 @@
+package Demo;
+
+public class Program2 {
+	public static void main(String[] args) {
+		String str="Ni13shIAvi34";
+		System.out.println(new Program2().getName(str));
+	}
+	String getName(String str) {
+		String st="";
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			if(Character.isUpperCase(ch))
+			{
+				ch=Character.toLowerCase(ch);
+			}
+			else if(Character.isLowerCase(ch))
+			{
+				ch=Character.toUpperCase(ch);
+			}
+			st+=ch;
+		}
+		return st;
+	}
+
+}
